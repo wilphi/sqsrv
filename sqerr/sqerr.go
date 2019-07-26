2f19c584a367edaba392fc19a0611940d0f8cad0
wilphi <37507463+wilphi@users.noreply.github.com>
2019-07-26 13:51:27 -0400

    Added formatting option for New errors

--- sqerr/sqerr.go
+++ sqerr/sqerr.go
@@ -1,5 +1,7 @@
 package sqerr
 
+import "fmt"
+
 // SQError type
 type SQError struct {
 	msg     string // description of error
@@ -11,9 +13,19 @@ func New(text string) error {
 	return &SQError{text, "Error"}
 }
 
+// Newf - Create a new Error with a formatted string
+func Newf(format string, a ...interface{}) error {
+	return &SQError{fmt.Sprintf(format, a...), "Error"}
+}
+
 // NewSyntax - Create a new Syntax Error
 func NewSyntax(text string) error {
 	return &SQError{text, "Syntax Error"}
+}
+
+// NewSyntaxf creates a new Syntax Error with the text formatted
+func NewSyntaxf(format string, a ...interface{}) error {
+	return &SQError{fmt.Sprintf(format, a...), "Syntax Error"}
 }
 
 // NewInternal - Create a new internal error
@@ -21,4 +33,10 @@ func NewInternal(text string) error {
 	return &SQError{text, "Internal Error"}
 
 }
+
+// NewInternalf - Create a new Internal Error with a formatted string
+func NewInternalf(format string, a ...interface{}) error {
+	return &SQError{fmt.Sprintf(format, a...), "Internal Error"}
+}
+
 func (e *SQError) Error() string { return e.errType + ": " + e.msg }
--- sqerr/sqerr_test.go
+++ sqerr/sqerr_test.go
@@ -30,7 +30,10 @@ func testErrsFunc(err error, expected string) func(*testing.T) {
 func TestSQErrs(t *testing.T) {
 
 	t.Run("Error type test", testErrsFunc(sqerr.New("Test Error"), "Error: Test Error"))
-	t.Run("Error type test", testErrsFunc(sqerr.NewSyntax("Test Error"), "Syntax Error: Test Error"))
-	t.Run("Error type test", testErrsFunc(sqerr.NewInternal("Test Error"), "Internal Error: Test Error"))
+	t.Run("Error type test with formatting", testErrsFunc(sqerr.Newf("Test %d Error %s", 2, "formatted"), "Error: Test 2 Error formatted"))
+	t.Run("Syntax Error type test", testErrsFunc(sqerr.NewSyntax("Test Error"), "Syntax Error: Test Error"))
+	t.Run("Syntax Error type test with formatting", testErrsFunc(sqerr.NewSyntaxf("Test %d Error %s", 4, "formatted-2"), "Syntax Error: Test 4 Error formatted-2"))
+	t.Run("Internal Error type test", testErrsFunc(sqerr.NewInternal("Test Error"), "Internal Error: Test Error"))
+	t.Run("Internal Error type test with formatting", testErrsFunc(sqerr.NewInternalf("Test %d Error %s", 6, "formatted-3"), "Internal Error: Test 6 Error formatted-3"))
 
 }
